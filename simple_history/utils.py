8995984d8a82f9811b345a1349b6d890a9c6f8ac
chairulqlub <chairul.anwar@qlub.io>
2023-07-04 22:08:20 +0700

    add update_fields param, only support for django >= 4.1

--- simple_history/utils.py
+++ simple_history/utils.py
@@ -66,6 +66,7 @@ def bulk_create_with_history(
     default_change_reason=None,
     default_date=None,
     update_conflicts=False,
+    update_fields=None,
 ):
     """
     Bulk create the objects specified by objs while also bulk creating
@@ -97,7 +98,7 @@ def bulk_create_with_history(
     second_transaction_required = True
     with transaction.atomic(savepoint=False):
         objs_with_id = model_manager.bulk_create(
-            objs, batch_size=batch_size, ignore_conflicts=ignore_conflicts, update_conflicts=update_conflicts
+            objs, batch_size=batch_size, ignore_conflicts=ignore_conflicts, update_conflicts=update_conflicts, update_fields=update_fields
         )
         if objs_with_id and objs_with_id[0].pk and (not ignore_conflicts or update_conflicts):
             second_transaction_required = False
